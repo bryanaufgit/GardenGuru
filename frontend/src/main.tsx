a476820a3829753a90ae4ef164975e3dff73400f
bryanaufgit <inbox@bryanpyka.de>
2025-04-16 14:07:38 +0200

    frontend and backend setup and basic API

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1 className="text-3xl font-bold text-green-600">🌱 GardenGuru läuft!</h1>
+    <h1 className="text-3xl font-bold text-green-600">🌱 GardenGuru läuft nicht du Haselnuss!</h1>
   </React.StrictMode>,
 )
